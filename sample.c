ee968cb461401c1f97205f725648c17970c8c6fa
4fvrRcPD7V <ishiwari.masuoka@gmail.com>
2020-03-14 19:04:09 +0900

    Second Commit

--- sample.c
+++ sample.c
@@ -4,5 +4,7 @@ int main(){
      printf("Git コマンド一覧\n");
      printf("git init : リポジトリを作成\n");
      printf("git status : Gitの状態を確認\n");
+     printf("git add : ファイルのステージ\n");
+     printf("git commit : コミットする \n");
      return 0;
 }
